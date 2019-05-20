e89b38f96e709a042693dc3b92887015e2c2a21b
Jeffrey Ellin <jellin@pivotal.io>
2019-05-20 19:44:15 -0400

    update ci

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -9,5 +9,7 @@ pushd source-code
 	mvn clean package 
 popd
 
-cp source-code/docker/Dockerfile source-code/target
-ls source-code/target
+mkdir packed-release/target
+cp source-code/docker/Dockerfile packed-release/target
+cp source-code/target/web-demo-0.0.1-SNAPSHOT.jar packed-release/target
+zip release-0.0.1.zip target/*
