15ea630ef09435709ba4af7d066b1e6af55dcbf2
Jeffrey Ellin <jellin@pivotal.io>
2019-05-20 20:03:27 -0400

    fix ci

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -13,4 +13,4 @@ popd
 mkdir packed-release/target
 cp source-code/docker/Dockerfile packed-release/target/
 cp source-code/target/web-demo-0.0.1-SNAPSHOT.jar packed-release/target/
-tar zcvf release-0.0.1.tgz -C target
+tar zcvf packed-release/release-0.0.1.tgz -C packed-release/target
