deb43f08969fb7aaea165e842a1594b6483eebf5
Jeffrey Ellin <jellin@pivotal.io>
2019-05-21 13:27:24 -0400

    cleanup

--- src/main/java/com/example/ellin/webdemo/WebDemoApplication.java
+++ src/main/java/com/example/ellin/webdemo/WebDemoApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebDemoApplication {
 
+	/**
+	 * Hello world spring app
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(WebDemoApplication.class, args);
 	}
