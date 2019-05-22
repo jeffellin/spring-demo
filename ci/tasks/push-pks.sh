f9f855c21e6ef9b42339e23330c2a34f4c642255
Jeffrey Ellin <jellin@pivotal.io>
2019-05-22 09:28:59 -0400

    fix concourse

---
+++ ci/tasks/push-pks.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+VERSION=2
+ls
+set -ex
+#ls release
+#cd release
+#tar xvf release-${VERSION}.tgz
+#pks login -a ${PKS_ENDPOINT} -u ${PKS_USER} -p ${PKS_PASSWORD} -k
+#pks get-credentials ${PKS_CLUSTER}
+kubectl get pods
+template=`cat "spring-demo-pod.yml" | sed "s/{VERSION}/$VERSION/g"`
+# apply the yml with the substituted value
+echo "$template" | kubectl apply -f -
