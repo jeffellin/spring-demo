dedc203bdca16f00d45544584ce1cba752f998e2
Jeffrey Ellin <jellin@pivotal.io>
2019-05-22 13:00:43 -0400

    update for k resource

--- ci/tasks/push-pks.sh
+++ ci/tasks/push-pks.sh
@@ -5,9 +5,9 @@ set -ex
 ls release
 cd release
 tar xvf release-${VERSION}.tgz
-pks login -a ${PKS_ENDPOINT} -u ${PKS_USER} -p ${PKS_PASSWORD} -k
-pks get-credentials ${PKS_CLUSTER}
-kubectl get pods
-template=`cat "spring-demo-pod.yml" | sed "s/{VERSION}/$VERSION/g"`
+# pks login -a ${PKS_ENDPOINT} -u ${PKS_USER} -p ${PKS_PASSWORD} -k
+# pks get-credentials ${PKS_CLUSTER}
+#kubectl get pods
+cat "spring-demo-pod.yml" | sed "s/{VERSION}/$VERSION/g" > spring-demo-modified.yml
 # apply the yml with the substituted value
-echo "$template" | kubectl apply -f -
+# echo "$template" | kubectl apply -f -
