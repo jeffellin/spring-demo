db8c72f02ec9b87c479a018138997430744cc11c
Jeffrey Ellin <jellin@pivotal.io>
2019-05-29 19:02:34 -0400

    update helm with version

--- ci/tasks/push-to-harbor.sh
+++ ci/tasks/push-to-harbor.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-echo "hello world"
+VERSION=$(cat release/version)
 
-#VERSION=$(cat release/version)
-ls
-set -ex
-#ls release
-#cd release
-#tar xvf release-${VERSION}.tgz
+sed 's/^\(\s*version\s*: \s*\).*/\1${VERSION}/' source-code/kubernetes/spring-demo/Chart.yaml 
+
 
 helm init --client-only
 helm repo add harbor  ${HELM_URL} --ca-file ${HELM_CA_PATH} --username ${HELM_USER} --password ${HELM_PASSWORD}
 helm push source-code/kubernetes/spring-demo harbor --ca-file source-code/kubernetes/harbor.pem  --username jeff --password 'Pivotal!23'
-
 # pks login -a ${PKS_ENDPOINT} -u ${PKS_USER} -p ${PKS_PASSWORD} -k
 # pks get-credentials ${PKS_CLUSTER}
 #kubectl get pods
