5bf9be6ca237d10927c38f5f3e975f156ce52ae9
Jeffrey Ellin <jellin@pivotal.io>
2019-05-29 17:41:39 -0400

    push via harbor

--- ci/tasks/push-pks.sh
+++ ci/tasks/push-pks.sh
@@ -11,6 +11,12 @@ tar xvf release-${VERSION}.tgz
 # pks login -a ${PKS_ENDPOINT} -u ${PKS_USER} -p ${PKS_PASSWORD} -k
 # pks get-credentials ${PKS_CLUSTER}
 #kubectl get pods
-template=`cat "spring-demo-pod.yml" | sed "s/{VERSION}/$VERSION/g"`
+
+
+#template=`cat "spring-demo-pod.yml" | sed "s/{VERSION}/$VERSION/g"`
 # apply the yml with the substituted value
-echo "$template" > ../pks-build/spring-demo-modified.yml
+#echo "$template" > ../pks-build/spring-demo-modified.yml
+
+helm init --client-only
+helm repo add harbor  ${HELM_URL} --ca-file ${HELM_CA_PATH} --username ${HELM_USER} --password ${HELM_PASSWORD}
+helm install harbor/spring-demo
