2cfd7484b4bb0e168f96f353ef6e7410ccc5a43b
[USERNAME] <jellin@pivotal.io>
2019-06-05 11:39:16 -0400

    truncate hash

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -4,7 +4,7 @@ set -eux
 export ROOT_FOLDER="$( pwd )"
 
 VERSION=$(cat source-code/.git/ref)
-VERSION=${VERSION:0-5}
+VERSION=${VERSION:0:6}
 SCRIPT_DIR=$(dirname "$0")
 M2_HOME="${ROOT_FOLDER}/.m2"
 M2_CACHE="${ROOT_FOLDER}/maven"
