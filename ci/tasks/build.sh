0f05cf9d67baf341dd6870c9b7bceddb254e2955
Jeffrey Ellin <jeff@ellin.com>
2019-12-20 13:23:48 -0500

    add commit to sh script

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -27,3 +27,8 @@ cp source-code/target/web-demo-0.0.1-SNAPSHOT.jar packed-release/target/web-demo
 cp -r source-code-gitops/. gitops
 sed -r  "s/^(\s*)(newTag\s*:.*$)/\1newTag: \"${VERSION}\"/" gitops/kubernetes/kustomization.yaml
 sed -r -i "s/^(\s*)(newTag\s*:.*$)/\1newTag: \"${VERSION}\"/" gitops/kubernetes/kustomization.yaml
+pushd gitops
+git config --global user.name "YOUR NAME"
+git config --global user.email "none@none.com"
+git add .
+git commit -m "update by ci"
