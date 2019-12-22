552a074b2ba5c67b4acd9756e751e9cd3c7c85e9
Jeffrey Ellin <jeff@ellin.com>
2019-12-22 14:32:57 -0500

    clean up for demo,  add color to metric for grafana

--- src/main/java/com/example/ellin/webdemo/WebDemoApplication.java
+++ src/main/java/com/example/ellin/webdemo/WebDemoApplication.java
@@ -1,78 +1,55 @@
 package com.example.ellin.webdemo;
+
 import io.micrometer.core.annotation.Timed;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.*;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 @SpringBootApplication
 public class WebDemoApplication {
 
-	/**
-	 * Hello world spring app.
-	 * 
-	 * @param args
-	 */
-	public static void main(final String[] args) {
-		SpringApplication.run(WebDemoApplication.class, args);
-	}
+    /**
+     * Hello world spring app.
+     *
+     * @param args
+     */
+    public static void main(final String[] args) {
+        SpringApplication.run(WebDemoApplication.class, args);
+    }
 
-	@RestController
-	@Timed
-	public class RestDemo {
+    @RestController
+    public class RestDemo {
 
-		private final Log log = LogFactory.getLog(getClass());
+        private final Log log = LogFactory.getLog(getClass());
 
-		@Autowired
-		MeterRegistry registry;
+        @Autowired
+        MeterRegistry registry;
 
-		@GetMapping("/")
-		public Map<String,String> sayHello() throws InterruptedException {
+        @GetMapping("/")
+        @Timed
+        public Map<String, String> sayHello() throws InterruptedException {
 
-			final Map<String,String> results = new HashMap<>();
-			final Counter counter = registry.counter("hello.count");
-			counter.increment();
-
-			final Random rn = new Random();
-			final int answer = rn.nextInt(5) + 1;
-
-			//Thread.sleep(answer * 1000);
-
-			results.put("string","helloworld");
-			results.put("color","yellow");
-			results.put("version","three");
-			results.put("counter", Integer.toString(answer));
-			return results;
+            final String color = "yello"
+            final Tag tag = Tag.of("color", color);
+            final Map<String, String> results = new HashMap<>();
+            final Counter counter = registry.counter("hello", Collections.singletonList(tag));
+            counter.increment();
+            results.put("string", "hell-world");
+            results.put("color", "yellow");
+            results.put("version", "three");
+            return results;
 
 
-			
-		}
-
-
-		@GetMapping("/hi")
-		public Map<String,String> sayHi() throws InterruptedException {
-
-			final Map<String,String> results = new HashMap<>();
-			final Counter counter = registry.counter("hello.count");
-			counter.increment();
-
-			final Random rn = new Random();
-			final int answer = rn.nextInt(5) + 1;
-
-			//Thread.sleep(answer * 1000);
-
-			results.put("string","hi");
-			results.put("color","yellow");
-			results.put("counter", Integer.toString(answer));
-			return results;
-
-		}
-
-	}
+        }
+    }
 }
