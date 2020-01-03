21764408514472f37258d33b519019b742861f8a
Oleg Iavorskyi <oleg.iavorskyi@gmail.com>
2020-01-03 10:49:33 -0500

    Change color to green

--- src/main/java/com/example/ellin/webdemo/WebDemoApplication.java
+++ src/main/java/com/example/ellin/webdemo/WebDemoApplication.java
@@ -47,7 +47,7 @@ public class WebDemoApplication {
 
             //Thread.sleep(5000);
 
-            final String color = "blue";
+            final String color = "green";
             final Tag tag = Tag.of("color", color);
             final Map<String, String> results = new HashMap<>();
             final Counter counter = registry.counter("hello", Collections.singletonList(tag));
